df4063002eb78c7e958234c732c42c519b1f2b23
masashiono0611 <masashi.ono.0611@gmail.com>
2025-07-30 18:04:26 +0900

    refactor: move TonContract to TonContract_sub and update .gitignore

--- TonContract/utils/Common.ts
+++
@@ -1,7 +0,0 @@
-import { Address, JettonMaster, TonClient } from '@ton/ton';
-
-export async function getJettonWalletAddr(tonClient: TonClient, masterAddr: Address, ownerAddr: Address) {
-    const openedMaster = tonClient.open(JettonMaster.create(masterAddr));
-    const walletAddr = await openedMaster.getWalletAddress(ownerAddr);
-    return walletAddr;
-}
